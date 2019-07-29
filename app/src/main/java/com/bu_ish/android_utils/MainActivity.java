73b448e2a159a3e0821e265de2621c6510b9384a
BuYishi <bu-ish@qq.com>
2019-07-29 23:56:21 +0800

    test CountdownTextView and LeakCanary

--- app/src/main/java/com/bu_ish/android_utils/MainActivity.java
+++ app/src/main/java/com/bu_ish/android_utils/MainActivity.java
@@ -1,14 +1,55 @@
 package com.bu_ish.android_utils;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private CountdownTextView ctv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ctv = findViewById(R.id.ctv);
+        ctv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ctv.start(10, new CountdownTextView.CountdownCallback() {
+                    @Override
+                    public void onStart() {
+                        ctv.setAlpha(0.3f);
+                    }
+
+                    @Override
+                    public void onTick(long secondsUntilFinished) {
+                        ctv.setText(secondsUntilFinished + "");
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        ctv.setAlpha(1);
+                        ctv.setText("获取验证码");
+                    }
+                });
+            }
+        });
+        testLeakCanary();
+    }
+
+    private void testLeakCanary() {
+        new CountDownTimer(60000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Toast.makeText(MainActivity.this, "测试LeakCanary", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        }.start();
     }
 }
