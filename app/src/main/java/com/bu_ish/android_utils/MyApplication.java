0cf660bd519f6b9f296f34cb71a5c7396f4a9565
BuYishi <bu-ish@qq.com>
2019-07-29 23:45:33 +0800

    LeakCanary.install

--- app/src/main/java/com/bu_ish/android_utils/MyApplication.java
+++ app/src/main/java/com/bu_ish/android_utils/MyApplication.java
@@ -2,5 +2,14 @@ package com.bu_ish.android_utils;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
+    }
 }
