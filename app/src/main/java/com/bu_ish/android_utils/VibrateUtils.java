8bb25c17fa809ca683c851018ad14c6f73c94093
unknown <bu-ish@qq.com>
2019-11-06 15:46:10 +0800

    update build.gralde in module app

--- app/src/main/java/com/bu_ish/android_utils/VibrateUtils.java
+++ app/src/main/java/com/bu_ish/android_utils/VibrateUtils.java
@@ -5,10 +5,12 @@ import android.os.Vibrator;
 
 import androidx.annotation.RequiresPermission;
 
+import java.util.Objects;
+
 public class VibrateUtils {
     @RequiresPermission(android.Manifest.permission.VIBRATE)
     public static void vibrate(Context context, long milliseconds) {
         Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(milliseconds);
+        Objects.requireNonNull(vibrator).vibrate(milliseconds);
     }
 }
