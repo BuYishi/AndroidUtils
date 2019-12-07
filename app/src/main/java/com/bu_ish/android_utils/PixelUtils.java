8ad6ed8beeabe4eac80d58494cecb93b3465788b
unknown <bu-ish@qq.com>
2019-12-07 17:54:06 +0800

    Arrange code in class PixelUtils

--- app/src/main/java/com/bu_ish/android_utils/PixelUtils.java
+++ app/src/main/java/com/bu_ish/android_utils/PixelUtils.java
@@ -5,32 +5,30 @@ import android.util.DisplayMetrics;
 
 public class PixelUtils {
     public static int dpToPx(Context context, float dp) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return (int) (dp * metrics.density + 0.5);
+        return (int) (dp * getDisplayMetrics(context).density + 0.5f);
     }
 
     public static int pxToDp(Context context, float px) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return (int) (px / metrics.density + 0.5);
+        return (int) (px / getDisplayMetrics(context).density + 0.5f);
     }
 
     public static int spToPx(Context context, float sp) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return (int) (sp * metrics.scaledDensity + 0.5);
+        return (int) (sp * getDisplayMetrics(context).scaledDensity + 0.5f);
     }
 
     public static int pxToSp(Context context, float px) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return (int) (px / metrics.scaledDensity + 0.5);
+        return (int) (px / getDisplayMetrics(context).scaledDensity + 0.5f);
     }
 
     public static int getDisplayWidth(Context context) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return metrics.widthPixels;
+        return getDisplayMetrics(context).widthPixels;
     }
 
     public static int getDisplayHeight(Context context) {
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return metrics.heightPixels;
+        return getDisplayMetrics(context).heightPixels;
+    }
+
+    private static DisplayMetrics getDisplayMetrics(Context context) {
+        return context.getResources().getDisplayMetrics();
     }
 }
