46ab598337262f6e340d4b7f9ad1f79afaf81a7d
unknown <bu-ish@qq.com>
2019-12-21 10:41:35 +0800

    Add function getBitmap to class BitmapUtils

--- app/src/main/java/com/bu_ish/android_utils/BitmapUtils.java
+++ app/src/main/java/com/bu_ish/android_utils/BitmapUtils.java
@@ -1,9 +1,12 @@
 package com.bu_ish.android_utils;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+
+import androidx.annotation.DrawableRes;
 
 public class BitmapUtils {
     public static Bitmap drawableToBitmap(Drawable drawable) {
@@ -15,5 +18,9 @@ public class BitmapUtils {
         drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
         drawable.draw(canvas);
         return bitmap;
+    }
+
+    public static Bitmap getBitmap(Context context, @DrawableRes int id) {
+        return drawableToBitmap(context.getResources().getDrawable(id));
     }
 }
