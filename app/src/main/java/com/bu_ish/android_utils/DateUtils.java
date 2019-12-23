afc09c7b8ec211441b109947f3f7ec45cc91eab3
unknown <bu-ish@qq.com>
2019-12-23 14:01:35 +0800

    Add functions format and timestampToString to class DateUtils

--- app/src/main/java/com/bu_ish/android_utils/DateUtils.java
+++ app/src/main/java/com/bu_ish/android_utils/DateUtils.java
@@ -2,12 +2,36 @@ package com.bu_ish.android_utils;
 
 import android.annotation.SuppressLint;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class DateUtils {
     public static String getCurrentTime(String pattern) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat(pattern);
-        return format.format(new Date());
+        return createSimpleDateFormat(pattern).format(new Date());
+    }
+
+    public static String format(String source, String sourcePattern, String destinationPattern) {
+        try {
+            SimpleDateFormat sourceFormat = createSimpleDateFormat(sourcePattern);
+            SimpleDateFormat destinationFormat = createSimpleDateFormat(destinationPattern);
+            return destinationFormat.format(Objects.requireNonNull(sourceFormat.parse(source)));
+        } catch (ParseException ex) {
+            return "";
+        }
+    }
+
+    public static String timestampToString(int timestampSecs, String pattern) {
+        return timestampToString(timestampSecs * 1000L, pattern);
+    }
+
+    public static String timestampToString(long timestampMillis, String pattern) {
+        return createSimpleDateFormat(pattern).format(new Date(timestampMillis));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private static SimpleDateFormat createSimpleDateFormat(String pattern) {
+        return new SimpleDateFormat(pattern);
     }
 }
